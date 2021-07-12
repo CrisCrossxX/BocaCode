ad7301061e2edd3fa3e75335abaf815d5fd0d67d
Chris De Leon <72676233+chrisdeleonn@users.noreply.github.com>
2021-07-12 08:59:57 -0400

    Create saturdayPrac.js
    Saturday

---
+++ saturdayPrac.js
@@ -0,0 +1,55 @@
+//function that decides if they're old enough to enter
+/*
+const personsAge = 21
+
+function isOldEnough(age) {
+    if (age < 21) {
+    console.log("sorry bro, you're too young, you can't enter");
+    }
+    else   {
+     console.log("you're good, come on in and have a good time")
+        }
+    }
+isOldEnough(personsAge) 
+*/
+
+//Object reference - multiple objects
+/*
+let person = [{
+    firstName: "Chris",
+    lastName: "De Leon",
+    age: 29,
+    likesCars: true
+}, {
+    homeTown: "Dallas",
+    status: "Budding Software Engineer",
+    otherLang: "Spanish"
+}]
+
+console.log("my age is ", person[0].age, "and I also know", person[1].otherLang)
+*/
+
+/*
+let typeOfDays = ["sunny", "rainy", "cold", "windy", "peachy"]
+
+console.log("today is", typeOfDays[4])
+*/
+
+//
+
+/*
+let numA = 1
+let numB = 2
+
+const istheTotal = (a, b) => {
+    console.log(a + b)
+}
+ istheTotal(numA, numB)
+ */
+
+ let arrayOfNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+ for(i = 1; i < arrayOfNum.length; i++)
+
+
+ console.log(arrayOfNum[i])
+
